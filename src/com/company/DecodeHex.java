769e6e5a61c7dc818e0bebf2d6702d67e7f4c3f2
Roman Cherepanov <rcherepanov@tektelic.com>
2022-08-10 22:43:31 +0300

    Init

---
+++ src/com/company/DecodeHex.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.util.Arrays;
+
+public class DecodeHex {
+    public static void main(String[] args) throws DecoderException {
+        byte[] decoded = Hex.decodeHex("01040000020202EF050400310604002F096500250A71FFE0FFE004100B6700D30B683F00FF016C0C6700D0");
+        System.out.println(Arrays.toString(decoded));
+    }
+}
---
+++ src/com/company/EncodeHex.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class EncodeHex {
+    public static void main(String[] args) {
+        System.out.println(new String(Hex.encodeHex(new byte[]{-3,69,14,5,118,51,-93,-1,4})).toUpperCase());
+    }
+}
